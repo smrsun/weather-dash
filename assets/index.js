12ca1c4a7770f60b11ff8b5adfed6eda746b5c98
smrsun4 <smrsun4@gmail.com>
2024-07-07 09:40:55 -0400

    adjusted code to try to make my functions work correctly and added the 5 day api

--- assets/index.js
+++ assets/index.js
@@ -12,14 +12,35 @@ function start() {
 
 function runApi(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
-   .then((res)=> {
+   .then((res) => {
     return res.json()
-  }).then((data)=> {
+  }).then((data) => {
     console.log(data);
+  
+    let iconEl = document.getElementById('icon');
+      iconEl.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+
+    let dateObj = new Date(data.dt * 1000);
+    let dateEl = document.getElementById('date');
+      dateEl.textContent = dateObj;
+
     const cityNameEl =  document.getElementById('cityName')
-    cityNameEl.textContent = data.name
-    citySearch.append(cityNameEl)
-  })
-}
+      cityNameEl.textContent = data.name
+  
+  });
+  
+
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(data);
+  
+      
+    
+   });
+  };
+
 
 citySearchBtn.addEventListener("click", start)
+
